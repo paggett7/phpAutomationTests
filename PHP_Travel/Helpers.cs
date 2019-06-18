31fc1b1ff330f303809680e62950237e0d7c14c6
Joshua Paggett <paggett7@gmail.com>
2019-06-18 19:51:04 -0400

    Commiting new clasees and tests.

--- PHP_Travel/Helpers.cs
+++ PHP_Travel/Helpers.cs
@@ -13,8 +13,27 @@ namespace PHP_Travel
         public static void CheckPageDisplayed(string pageTitle)
         {
             var page = driver.FindElement(By.CssSelector("h1.page-heading")).Text;
-
             Assert.IsTrue(page == pageTitle);
+        }
+
+        public class MenuToolbar
+        {
+            public static void Women()
+            {
+                driver.FindElement(By.XPath("//a[@title='Women']")).Click();
+            }
+
+            public static void Dresses()
+            {
+
+                driver.FindElements(By.XPath("//a[@title='Dresses' and @class='sf-with-ul']")).Last().Click();
+            }
+
+            public static void TShirts()
+            {
+
+                driver.FindElement(By.XPath("//li[@class='']//a[ @title='T-shirts']")).Click();
+            }
         }
     }
 }
